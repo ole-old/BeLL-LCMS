2ba2e395892ef61bc399419094e7599e0f977a45
rjsteinert <rj@rjsteinert.com>
2013-04-21 15:32:46 +0000

    Attachments now submitting.

--- attachments/app/views/ResourceForm.js
+++ attachments/app/views/ResourceForm.js
@@ -7,26 +7,43 @@ $(function() {
       },
 
       initialize: function() {
-        this.model = new Resource();
+        this.model = new Resource()
       },
 
       render: function() {
-        var $button = $('<button type="button" name="save" id="formButton">save</button>');
 
-        this.form = new Backbone.Form({ model: this.model });
+        // Extra elements not covered in the schema
+        var $fileEl = $('<form method="post" id="fileAttachment"><input type="file" name="_attachments" id="_attachments" multiple="multiple" /> <input class="rev" type="hidden" name="_rev"></form>')
+        var $button = $('<button type="button" name="save" id="formButton">save</button>')
 
-        this.$el.append(this.form.render().el);
-        this.$el.append($button);
+        this.form = new Backbone.Form({ model: this.model })
+
+        this.$el.append(this.form.render().el)
+        this.$el.append($fileEl)
+        this.$el.append($button)
 
         return this;
       },
 
       saveForm: function() {
-          this.form.commit();
-          this.model.save();
+          // Put the form's input into the model in memory
+          this.form.commit()
+          // Send the updated model to the server
+          this.model.save(null, {success: function(model, response, options) {
+            // Now that the model is saved as a couch doc, we can attach a new file to the couch doc
+            $("form#fileAttachment .rev").attr('value', response._rev)
+            $('form#fileAttachment').ajaxSubmit({
+              url: "/"+ model._db.name +"/"+ response._id,
+              success: function(response) {
+                alert("Your resource has been submitted.")
+              }
+            })
+          },
+          error: function() {
+            alert('nope')
+          }})
 
-          alert('hit saveForm');
-          console.log(this.model.toJSON());
+          console.log(this.model.toJSON())
       }
     });
 
